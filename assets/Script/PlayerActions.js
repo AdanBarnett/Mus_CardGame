f4e37703f543440f44c90341d678f8843433aed6
EliteDev0227 <59699555+EliteDev0227@users.noreply.github.com>
2023-06-01 15:55:00 -0400

    Add Bet more popup inside PlayerActions Add BlockInput component to TopBar and chat area

--- assets/Script/PlayerActions.js
+++ assets/Script/PlayerActions.js
@@ -5,12 +5,18 @@ export default cc.Class({
     buttonRoot: cc.Node,
     musButtonRoot: cc.Node,
     betButtonRoot: cc.Node,
+    betPopupRoot: cc.Node,
+    betAmountLabel: cc.Label,
+
+    _amount: 2,
   },
 
   // onLoad () {},
 
   start() {
     this.showButtonRoot(false);
+    this.showBetPopup(false);
+    this.updateBetAmountLabel();
   },
 
   showMusButtons() {
@@ -47,6 +53,44 @@ export default cc.Class({
 
   onUserRaiseClick() {
     console.log("onUserRaiseClick");
+    this.showBetPopup(true);
+  },
+
+  // Bet Popup Section
+  showBetPopup(show) {
+    this.betPopupRoot.active = show;
+  },
+
+  updateBetAmountLabel() {
+    this.betAmountLabel.string = this._amount;
+  },
+
+  onUserMinusClick() {
+    if (this._amount > 2) {
+      this._amount--;
+      this.updateBetAmountLabel();
+    }
+  },
+
+  onUserPlusClick() {
+    if (this._amount < 10) {
+      this._amount++;
+      this.updateBetAmountLabel();
+    }
+  },
+
+  onUserBetOkClick() {
+    console.log("onUserBetOkClick", this._amount);
+    this.showBetPopup(false);
+  },
+
+  onUserOrdagoClick() {
+    console.log("onUserOrdagoClick");
+    this.showBetPopup(false);
+  },
+
+  onUserBackAlphaClick() {
+    this.showBetPopup(false);
   },
 
   // update (dt) {},
